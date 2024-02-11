02d6515518f1cea27380c8a35c6437e84f1204c7
powerspieler <pascal.houy@web.de>
2024-02-11 01:24:05 +0100

    Fixed Armorstand losing arms when breaked

--- src/main/java/de/powerspieler/tmswadditions/armorstand/Armorstand.java
+++ src/main/java/de/powerspieler/tmswadditions/armorstand/Armorstand.java
@@ -1,12 +1,17 @@
 package de.powerspieler.tmswadditions.armorstand;
 
+import com.destroystokyo.paper.inventory.meta.ArmorStandMeta;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Optional;
 
 public class Armorstand implements Listener {
 
@@ -16,9 +21,26 @@ public class Armorstand implements Listener {
             ArmorStand armorStand = (ArmorStand) event.getRightClicked();
             if(!armorStand.hasArms()){
                 armorStand.setArms(true);
-                ItemStack stick = event.getPlayer().getInventory().getItemInMainHand();
-                stick.setAmount(stick.getAmount() - 1);
+                if(!(event.getPlayer().getGameMode() == GameMode.CREATIVE)){
+                    ItemStack stick = event.getPlayer().getInventory().getItemInMainHand();
+                    stick.setAmount(stick.getAmount() - 1);
+                }
                 event.setCancelled(true); // Prevent giving a second stick
+            }
+        }
+    }
+
+    @EventHandler
+    public void onArmorStandBreak(EntityDeathEvent event){
+        if(event.getEntity() instanceof ArmorStand armorStand && armorStand.hasArms()){
+            Optional<ItemStack> optionalItemStack = event.getDrops().stream().filter(item -> item.getType() == Material.ARMOR_STAND).findFirst();
+            if(optionalItemStack.isPresent()){
+                ItemStack item = optionalItemStack.get();
+                event.getDrops().remove(item);
+                ArmorStandMeta itemMeta = (ArmorStandMeta) item.getItemMeta();
+                itemMeta.setShowArms(true);
+                item.setItemMeta(itemMeta);
+                event.getDrops().add(item);
             }
         }
     }
